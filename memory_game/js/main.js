a9417113cc25a67edc928cd3dd383a106c20adda
mohammed alhammad <someone@someplace.com>
2019-02-18 21:35:39 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,33 @@
 // console.log("Up and running!");
 
-var cards =["queen","queen","king","king"];
-var cardsInPlay= cards[0];
+var cards =[
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+
+},
+
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
+},
+
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"
+}
+
+];
+var cardsInPlay= cards[0].rank;
 
 
 // var cardOne =cards[0];
@@ -28,11 +54,16 @@ console.log("Sorry, try again.");
 
 var cardOne = cards[0];
 cards.push(cardOne);
-console.log("User flipped " + cardOne);
+console.log("User flipped " + cards[0].rank);
+console.log(cards[0].cardImage);
+console.log(cards[0].suit);
 
 var cardTwo = cards[2];
 cards.push(cardTwo);
-console.log("User flipped " + cardTwo);
+console.log("User flipped " + cards[2].rank);
+console.log(cards[2].cardImage);
+console.log(cards[2].suit);
+
 
 
 
